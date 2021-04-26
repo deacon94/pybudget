7919ebf5ef431e8cefa20881b97e454417bea5f1
deacon94 <mrich1834@gmail.com>
2021-04-26 17:05:59 -0400

    Starting Transfer method

--- budget.py
+++ budget.py
@@ -6,12 +6,38 @@ class Category:
         self.ledger = list()
 
     def deposit(self, amount, description=''):
-        # Add amount to category_amount
         if (amount > 0):
+            # Add amount to category_amount (for easy calculations)
             self.category_amount = self.category_amount + amount 
-            # self.ledger.append
 
-# ledger = list of dictionary objects {"amount": amount, "description": description}
+            # Adjust ledger
+            self.ledger.append({'amount': amount, 'description': description})
+
+    def check_funds(self, amount):
+        # Check to see if the amount can be removed (enough $ exists to cover)
+        if (self.category_amount >= amount):
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount, description=''):
+        # Confirm there are enough funds to make withdraw
+        if (amount > 0) and (self.check_funds(amount)):
+            # Subtract amount to category_amount (for easy calculations)
+            self.category_amount = self.category_amount - amount 
+
+            # Adjust ledger
+            neg_amount = amount * -1
+            self.ledger.append({'amount': neg_amount, 'description': description})
+
+            return True
+
+        # Not enough funds
+        return False
+
+    def transfer(self, amount, dest_category):
+        
+
 
     def get_balance(self):
         return self.category_amount
